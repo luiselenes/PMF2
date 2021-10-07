004548411d6aa62b17fc88f9371e503109b3d9bd
luiselenes <luiseelenes@outlook.com>
2021-10-07 17:59:10 -0600

    modelos

--- mfapi/config/routes.rb
+++ mfapi/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :imagens
+  resources :routes
+  resources :green_houses
+  resources :devices
   resources :agricultural_companies
   root :to => "app#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
